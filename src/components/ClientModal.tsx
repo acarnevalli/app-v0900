b100151fe3569e720e89f1231490d11e8cb2c5b1
acarnevalli <49988241+acarnevalli@users.noreply.github.com>
2025-10-08 09:25:21 -0300

    Update ClientModal.tsx

--- src/components/ClientModal.tsx
+++ src/components/ClientModal.tsx
@@ -100,7 +100,7 @@ const ClientModal: React.FC<ClientModalProps> = ({ client, onClose }) => {
       }
     }));
 
-    if (zipCode.length === 8) {
+    if (zipCode && zipCode.length === 8) {
       try {
         const response = await fetch(`https://viacep.com.br/ws/${zipCode}/json/`);
         const data = await response.json();
