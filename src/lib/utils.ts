d6ee934f59704036388b201e124c15cf25498310
acarnevalli <49988241+acarnevalli@users.noreply.github.com>
2025-10-16 18:19:22 -0300

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -85,3 +85,16 @@ export const validateCNPJ = (cnpj: string): boolean => {
 
   return true;
 };
+
+export const formatCurrency = (value: number): string => {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+};
+
+export const formatDate = (date: string | Date): string => {
+  return new Intl.DateTimeFormat('pt-BR').format(
+    typeof date === 'string' ? new Date(date) : date
+  );
+};
